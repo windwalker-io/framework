42fac94aed7afe037f425802a61504d9eff549df
Asika <asika32764@gmail.com>
2013-04-16 19:54:06 +0800

    Uri Add download

--- helpers/uri.php
+++ helpers/uri.php
@@ -74,6 +74,62 @@ class AKHelperUri {
 		return $url ;
 	}
 	
+	
+	/*
+	 * function download
+	 * @param $stream
+	 */
+	
+	public static function download($path, $absolute = false, $stream = false, $option = array())
+	{
+		if($stream) {
+			if(!$absolute){
+				$path = JPATH_ROOT . '/' . $path ;
+			}
+			
+			if(!JFile::exists($path)) die() ;
+			
+			$file = pathinfo($path) ;
+			
+			$filesize = filesize($path) + JArrayHelper::getValue($option, 'size_offset', 0); 
+			ini_set('memory_limit', JArrayHelper::getValue($option, 'memory_limit', '1540M') );
+			
+			// Set Header
+			header('Content-Type: application/octet-stream');
+			header('Cache-Control: no-store, no-cache, must-revalidate');
+			header('Cache-Control: pre-check=0, post-check=0, max-age=0');
+			header('Content-Transfer-Encoding: binary');
+			header('Content-Encoding: none');
+			header('Content-type: application/force-download');
+			header('Content-length: '.$filesize);
+			header('Content-Disposition: attachment; filename="' . $file['basename'] . '"');
+			
+			$handle 	= fopen($path, 'rb'); 
+			$buffer 	= '';
+			$chunksize 	= 1 * (1024 * 1024);
+			
+			// Start Download File
+			while (!feof($handle)) { 
+			  $buffer = fread($handle, $chunksize); 
+			  echo $buffer; 
+			  ob_flush(); 
+			  flush(); 
+			}
+			
+			fclose($handle); 
+			
+			jexit();
+		}else{
+			if(!$absolute){
+				$path = JURI::root().$path;
+			}
+			
+			$app = JFactory::getApplication() ;
+			$app->redirect( $path );
+		}
+	}
+	
+	
 	public static function current( $hasQuery = false ) {
 		if( $hasQuery )
 			return JFactory::getURI()->toString();
