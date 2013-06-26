5c12d2aba2327181079dc0575c61ab95f8f50a8e
Asika <asika32764@gmail.com>
2013-06-26 18:11:43 +0800

    Hide dotFiles

--- helpers/elfinder.php
+++ helpers/elfinder.php
@@ -197,11 +197,10 @@ SCRIPT;
 		 * @return bool|null
 		 **/
 		function access($attr, $path, $data, $volume) {
-            $r = array();
-			$r[] = strpos(basename($path), '.') === 0       // if file/folder begins with '.' (dot)
-				? !($attr == 'read' || $attr == 'write')    // set read+write to false, other (locked+hidden) set to true
-				:  null;                                    // else elFinder decide it itself
-		}
+            return strpos(basename($path), '.') === 0       // if file/folder begins with '.' (dot)
+                ? !($attr == 'read' || $attr == 'write')    // set read+write to false, other (locked+hidden) set to true
+                :  null;                                    // else elFinder decide it itself
+        }
 		
         
         // Get Some Request
