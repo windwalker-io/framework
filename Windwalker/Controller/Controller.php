8a7ab6dd83ebc6cca50e4fe01e5a535e21a35419
Simon Asika <asika32764@gmail.com>
2013-12-22 14:21:07 +0800

    Update with Easyset

---
+++ Windwalker/Controller/Controller.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Part of Windwalker RAD framework package.
+ *
+ * @author     Simon Asika <asika32764@gmail.com>
+ * @copyright  Copyright (C) 2014 Asikart. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Windwalker\Controller;
+
+/**
+ * Class Controller
+ *
+ * @since 2.0
+ */
+class Controller extends \JControllerLegacy
+{
+}
---
+++ Windwalker/autoload.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Part of Windwalker RAD framework package.
+ *
+ * @author     Simon Asika <asika32764@gmail.com>
+ * @copyright  Copyright (C) 2014 Asikart. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+JLoader::registerNamespace('Windwalker', dirname(__DIR__));
