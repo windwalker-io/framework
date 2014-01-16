210daa27885c912ccc64c774e104dc084c35975e
Simon Asika <asika32764@gmail.com>
2014-01-17 00:18:26 +0800

    DataMapper

---
+++ DataSet.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Part of Windwalker project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Windwalker\Data;
+
+use JDataSet;
+
+/**
+ * Class DataSet
+ *
+ * @since 1.0
+ */
+class DataSet extends JDataSet
+{
+}
