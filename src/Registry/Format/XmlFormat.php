c9d4975eda80ef20b7d1b313e924e2cf1790c035
Simon Asika <asika32764@gmail.com>
2014-08-02 14:01:01 +0800

    Update file headers

--- src/Registry/Format/XmlFormat.php
+++ src/Registry/Format/XmlFormat.php
@@ -2,7 +2,7 @@
 /**
  * Part of Windwalker project.
  *
- * @copyright  Copyright (C) 2014 {ORGANIZATION}. All rights reserved.
+ * @copyright  Copyright (C) 2008 - 2014 Asikart.com. All rights reserved.
  * @license    GNU General Public License version 2 or later;
  */
 
