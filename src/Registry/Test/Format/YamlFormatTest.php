8e464f16f20c0b9ae1e95f97b7d39a0309df8806
Simon Asika <asika32764@gmail.com>
2014-08-10 17:55:43 +0800

    Add format test but imcompleted

---
+++ src/Registry/Test/Format/YamlFormatTest.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Part of Windwalker project Test files.
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Windwalker\Registry\Test\Format;
+
+use Windwalker\Registry\Format\YamlFormat;
+
+/**
+ * Test class of YamlFormat
+ *
+ * @since {DEPLOY_VERSION}
+ */
+class YamlFormatTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * Test instance.
+	 *
+	 * @var YamlFormat
+	 */
+	protected $instance;
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @return void
+	 */
+	protected function setUp()
+	{
+		$this->instance = new YamlFormat;
+	}
+
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @return void
+	 */
+	protected function tearDown()
+	{
+	}
+
+	/**
+	 * Method to test objectToString().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Registry\Format\YamlFormat::objectToString
+	 * @TODO   Implement testObjectToString().
+	 */
+	public function testObjectToString()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test stringToObject().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Registry\Format\YamlFormat::stringToObject
+	 * @TODO   Implement testStringToObject().
+	 */
+	public function testStringToObject()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test getParser().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Registry\Format\YamlFormat::getParser
+	 * @TODO   Implement testGetParser().
+	 */
+	public function testGetParser()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test setParser().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Registry\Format\YamlFormat::setParser
+	 * @TODO   Implement testSetParser().
+	 */
+	public function testSetParser()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test getDumper().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Registry\Format\YamlFormat::getDumper
+	 * @TODO   Implement testGetDumper().
+	 */
+	public function testGetDumper()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test setDumper().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Registry\Format\YamlFormat::setDumper
+	 * @TODO   Implement testSetDumper().
+	 */
+	public function testSetDumper()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
