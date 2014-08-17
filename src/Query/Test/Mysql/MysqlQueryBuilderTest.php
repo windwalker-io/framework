b94aafea5ae997ed3571e5b6de6f069e8e79ecb4
Simon Asika <asika32764@gmail.com>
2014-08-17 18:19:15 +0800

    Add incompleted mysqlQueryBuilder test

---
+++ src/Query/Test/Mysql/MysqlQueryBuilderTest.php
@@ -0,0 +1,305 @@
+<?php
+/**
+ * Part of Windwalker project Test files.
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Windwalker\Query\Test\Mysql;
+
+/**
+ * Test class of MysqlQueryBuilder
+ *
+ * @since {DEPLOY_VERSION}
+ */
+class MysqlQueryBuilderTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * Method to test showDatabases().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::showDatabases
+	 * @TODO   Implement testShowDatabases().
+	 */
+	public function testShowDatabases()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test createDatabase().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::createDatabase
+	 * @TODO   Implement testCreateDatabase().
+	 */
+	public function testCreateDatabase()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test dropDatabase().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::dropDatabase
+	 * @TODO   Implement testDropDatabase().
+	 */
+	public function testDropDatabase()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test showTableColumns().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::showTableColumns
+	 * @TODO   Implement testShowTableColumns().
+	 */
+	public function testShowTableColumns()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test showDbTables().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::showDbTables
+	 * @TODO   Implement testShowDbTables().
+	 */
+	public function testShowDbTables()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test createTable().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::createTable
+	 * @TODO   Implement testCreateTable().
+	 */
+	public function testCreateTable()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test dropTable().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::dropTable
+	 * @TODO   Implement testDropTable().
+	 */
+	public function testDropTable()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test alterColumn().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::alterColumn
+	 * @TODO   Implement testAlterColumn().
+	 */
+	public function testAlterColumn()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test addColumn().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::addColumn
+	 * @TODO   Implement testAddColumn().
+	 */
+	public function testAddColumn()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test changeColumn().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::changeColumn
+	 * @TODO   Implement testChangeColumn().
+	 */
+	public function testChangeColumn()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test modifyColumn().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::modifyColumn
+	 * @TODO   Implement testModifyColumn().
+	 */
+	public function testModifyColumn()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test dropColumn().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::dropColumn
+	 * @TODO   Implement testDropColumn().
+	 */
+	public function testDropColumn()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test addIndex().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::addIndex
+	 * @TODO   Implement testAddIndex().
+	 */
+	public function testAddIndex()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test buildIndexDeclare().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::buildIndexDeclare
+	 * @TODO   Implement testBuildIndexDeclare().
+	 */
+	public function testBuildIndexDeclare()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test dropIndex().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::dropIndex
+	 * @TODO   Implement testDropIndex().
+	 */
+	public function testDropIndex()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test build().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::build
+	 * @TODO   Implement testBuild().
+	 */
+	public function testBuild()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test replace().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::replace
+	 * @TODO   Implement testReplace().
+	 */
+	public function testReplace()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+
+	/**
+	 * Method to test getQuery().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\Query\Mysql\MysqlQueryBuilder::getQuery
+	 * @TODO   Implement testGetQuery().
+	 */
+	public function testGetQuery()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
+}
