236b975ba2e753885ff4a6bd8989cd3550da9b4a
Simon Asika <asika32764@gmail.com>
2014-09-13 14:07:15 +0800

    Update column

--- src/Database/Command/Table/Column.php
+++ src/Database/Command/Table/Column.php
@@ -23,6 +23,70 @@ class Column
 	protected $name = null;
 
 	/**
+	 * Property type.
+	 *
+	 * @var  string
+	 */
+	protected $type;
+
+	/**
+	 * Property unsigned.
+	 *
+	 * @var  bool
+	 */
+	protected $unsigned;
+
+	/**
+	 * Property notNull.
+	 *
+	 * @var  bool
+	 */
+	protected $notNull;
+
+	/**
+	 * Property default.
+	 *
+	 * @var  string
+	 */
+	protected $default;
+
+	/**
+	 * Property position.
+	 *
+	 * @var  string
+	 */
+	protected $position;
+
+	/**
+	 * Property comment.
+	 *
+	 * @var  string
+	 */
+	protected $comment;
+
+	/**
+	 * Class init.
+	 *
+	 * @param string $name
+	 * @param string $type
+	 * @param bool   $unsigned
+	 * @param bool   $notNull
+	 * @param string $default
+	 * @param null   $position
+	 * @param string $comment
+	 */
+	public function __construct($name, $type = 'text', $unsigned = false, $notNull = false, $default = '', $position = null, $comment = '')
+	{
+		$this->name = $name;
+		$this->type = $type;
+		$this->unsigned = $unsigned;
+		$this->notNull = $notNull;
+		$this->default = $default;
+		$this->position = $position;
+		$this->comment = $comment;
+	}
+
+	/**
 	 * Method to get property Name
 	 *
 	 * @return  string
