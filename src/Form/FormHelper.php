c3ea22b3619d57af16f0b3340f711915fe5dc73c
Simon Asika <asika32764@gmail.com>
2015-03-04 21:44:17 +0800

    Update FormHelper.php

--- src/Form/FormHelper.php
+++ src/Form/FormHelper.php
@@ -97,11 +97,11 @@ class FormHelper
 
 		foreach ($args as $arg)
 		{
-			if (is_object($dataTmp) && !empty($dataTmp->$arg))
+			if (is_object($dataTmp) && isset($dataTmp->$arg))
 			{
 				$dataTmp = $dataTmp->$arg;
 			}
-			elseif (is_array($dataTmp) && !empty($dataTmp[$arg]))
+			elseif (is_array($dataTmp) && isset($dataTmp[$arg]))
 			{
 				$dataTmp = $dataTmp[$arg];
 			}
