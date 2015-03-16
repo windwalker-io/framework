1761f806a589356ef501b88f682c471006e13e72
Simon Asika <asika32764@gmail.com>
2015-03-16 11:28:56 +0800

    Test php 7

--- build/travis/subtree/dotest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd src/$1;
-echo "Start Test: $1";
-echo '----------------------------------------------------------------------------------------';
-composer update;
-phpunit --configuration phpunit.travis.xml;
-cd ../..;
