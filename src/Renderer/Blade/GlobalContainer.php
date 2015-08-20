bd901cc0286f9c499e779ab4289523eeb357963e
Simon Asika <asika32764@gmail.com>
2015-08-20 12:18:07 +0800

    Add blade custom tags

---
+++ src/Renderer/Blade/GlobalContainer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) 2015 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace Windwalker\Renderer\Blade;
+
+/**
+ * The GlobalContainer class.
+ *
+ * @since  {DEPLOY_VERSION}
+ */
+class GlobalContainer
+{
+
+}
