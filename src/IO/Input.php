7112b1ab63b8d4490417c90ed60d1c4b5cdd55a3
Simon Asika <asika32764@gmail.com>
2016-03-08 00:34:44 +0800

    Update input

--- src/IO/Input.php
+++ src/IO/Input.php
@@ -159,7 +159,9 @@ class Input implements \Serializable, \Countable
 
 		if (in_array(strtolower($name), array('put', 'patch', 'delete', 'link', 'unlink')))
 		{
-			$this->inputs[$name] = new FormDataInput(null, $filter);
+			$data = (strtolower($this->getMethod()) == strtolower($name)) ? null : array();
+
+			$this->inputs[$name] = new FormDataInput($data, $filter);
 
 			return $this->inputs[$name];
 		}
@@ -556,5 +558,19 @@ class Input implements \Serializable, \Countable
 		}
 
 		return $return;
+	}
+
+	/**
+	 * Method to set property data
+	 *
+	 * @param   array $data
+	 *
+	 * @return  static  Return self to support chaining.
+	 */
+	public function setData($data)
+	{
+		$this->data = $data;
+
+		return $this;
 	}
 }
