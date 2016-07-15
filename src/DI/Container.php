c580911f8968499895f197bd2716058e036705db
Simon Asika <asika32764@gmail.com>
2016-07-15 14:10:43 +0800

    Fix 5.3

--- src/DI/Container.php
+++ src/DI/Container.php
@@ -319,7 +319,7 @@ class Container implements \ArrayAccess, \IteratorAggregate, \Countable
 				}
 				// Otherwise we create this object recursive
 				elseif (!$dependency->isAbstract() && !$dependency->isInterface() &&
-					(version_compare(PHP_VERSION, '5.4', '>=') && !$dependency->isTrait()))
+					!(version_compare(PHP_VERSION, '5.4', '>=') && $dependency->isTrait()))
 				{
 					// Find child args if set
 					if (isset($args[$dependencyClassName]) && is_array($args[$dependencyClassName]))
