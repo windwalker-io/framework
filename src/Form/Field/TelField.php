d9b8ff1f210ad81d75ccfe923ea5d44024d9a65a
Simon Asika <asika32764@gmail.com>
2016-10-10 09:40:34 +0000

    Prepare for 3.0.1 release.

--- src/Form/Field/TelField.php
+++ src/Form/Field/TelField.php
@@ -2,7 +2,7 @@
 /**
  * Part of earth project.
  *
- * @copyright  Copyright (C) 2016 {ORGANIZATION}. All rights reserved.
+ * @copyright  Copyright (C) 2016 LYRASOFT. All rights reserved.
  * @license    GNU General Public License version 2 or later.
  */
 
@@ -11,7 +11,7 @@ namespace Windwalker\Form\Field;
 /**
  * The NumberField class.
  *
- * @since  {DEPLOY_VERSION}
+ * @since  3.0.1
  */
 class TelField extends AbstractHtml5Field
 {
