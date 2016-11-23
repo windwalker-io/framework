21e3465058269d6edd993d2ad3b84cddee1fb17d
Simon Asika <asika32764@gmail.com>
2016-11-23 10:59:58 +0800

    Init

---
+++ src/String/StringHelper.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Part of ww4 project.
+ *
+ * @copyright  Copyright (C) 2016 LYRASOFT.
+ * @license    Please see LICENSE file.
+ */
+
+namespace Windwalker\String;
+
+/**
+ * The StringHelper class.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class StringHelper
+{
+
+}
