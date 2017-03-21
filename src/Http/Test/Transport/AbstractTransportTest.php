b0f3bd18ebe1fa1b07bbc0256f70e21b2d133959
Simon Asika <asika32764@gmail.com>
2017-03-21 14:37:12 +0800

    Fix unittest

--- src/Http/Test/Transport/AbstractTransportTest.php
+++ src/Http/Test/Transport/AbstractTransportTest.php
@@ -101,6 +101,8 @@ abstract class AbstractTransportTest extends \PHPUnit\Framework\TestCase
 
 		$response = $this->instance->request($request);
 
+		show($response->getBody()->__toString());
+
 		$this->assertEquals(200, $response->getStatusCode());
 		$this->assertJson($response->getBody()->getContents());
 
