ee72fe67d8d613f47bfaa0ecc0f5d217e0a600e4
Simon Asika <asika32764@gmail.com>
2017-03-21 15:29:13 +0800

    Fix php 5.5 test

--- src/Crypt/Cipher/AbstractCipher.php
+++ src/Crypt/Cipher/AbstractCipher.php
@@ -105,9 +105,10 @@ abstract class AbstractCipher implements CipherInterface
 		}
 
 		$this->privateKey = $key;
-		$this->options = array_merge([
+		$this->options = array_merge(
+			[
 			'pbkdf2_iteration' => 12000
-		], $options);
+			], $options);
 	}
 
 	/**
@@ -406,13 +407,44 @@ abstract class AbstractCipher implements CipherInterface
 			trigger_error('PBKDF2 ERROR: Invalid parameters.', E_USER_ERROR);
 		}
 
-		// The output length is in NIBBLES (4-bits) if $raw_output is false!
-		if (!$raw_output)
+		if (function_exists('hash_pbkdf2'))
 		{
-			$key_length *= 2;
+			// The output length is in NIBBLES (4-bits) if $raw_output is false!
+			if (!$raw_output)
+			{
+				$key_length *= 2;
+			}
+
+			return hash_pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output);
 		}
 
-		return hash_pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output);
+		$hash_length = strlen(hash($algorithm, '', true));
+		$block_count = ceil($key_length / $hash_length);
+
+		$output = '';
+
+		for ($i = 1; $i <= $block_count; $i++)
+		{
+			// $i encoded as 4 bytes, big endian.
+			$last = $salt . pack('N', $i);
+			// first iteration
+			$last = $xorsum = hash_hmac($algorithm, $last, $password, true);
+			// perform the other $count - 1 iterations
+			for ($j = 1; $j < $count; $j++)
+			{
+				$xorsum ^= ($last = hash_hmac($algorithm, $last, $password, true));
+			}
+			$output .= $xorsum;
+		}
+
+		if ($raw_output)
+		{
+			return substr($output, 0, $key_length);
+		}
+		else
+		{
+			return bin2hex(substr($output, 0, $key_length));
+		}
 	}
 
 	/**
