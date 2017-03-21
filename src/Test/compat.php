9444c9cdd6f05d20b0dce25ed761ce1f198f654d
Simon Asika <asika32764@gmail.com>
2017-03-21 11:10:44 +0800

    Add class check

--- src/Test/compat.php
+++ src/Test/compat.php
@@ -6,7 +6,7 @@
  * @license    __LICENSE__
  */
 
-if (!class_exists('PHPUnit\Framework\TestCase'))
+if (!class_exists('PHPUnit\Framework\TestCase') && class_exists('PHPUnit_Framework_TestCase'))
 {
 	class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
 }
