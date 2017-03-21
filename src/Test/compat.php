500933008a46516e3fc97a411280ec55c4f882fa
Simon Asika <asika32764@gmail.com>
2017-03-21 10:06:13 +0800

    Update phpunit compat

---
+++ src/Test/compat.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) 2017 ${ORGANIZATION}.
+ * @license    __LICENSE__
+ */
+
+if (!class_exists('PHPUnit\Framework\TestCase'))
+{
+	class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+}
