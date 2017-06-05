db47bddfb104a3f9f14f9399689f51f8be282061
Simon Asika <asika32764@gmail.com>
2017-06-06 02:02:02 +0800

    Fix test

--- src/Structure/Test/Stubs/flower.php
+++ src/Structure/Test/Stubs/flower.php
@@ -1,17 +1,17 @@
 <?php
 
-return [
+return array(
 	'flower' => 'sakura',
 	'olive' => 'peace',
-	'pos1' => [
+	'pos1' => array(
 		"sunflower" => "love"
-	],
-	'pos2' => [
+	),
+	'pos2' => array(
 		"cornflower" => "elegant"
-	],
-	'array' => [
+	),
+	'array' => array(
 		"0" => "A",
 		"1" => "B",
 		"2" => "C"
-	],
-];
+	),
+);
