9330ae115a2bb88097f11684dffce977c1e5523b
Simon Asika <asika32764@gmail.com>
2019-02-04 00:11:39 +0800

    Move tap()

--- src/Utilities/functions.php
+++ src/Utilities/functions.php
@@ -6,94 +6,115 @@
  * @license    LGPL-2.0-or-later
  */
 
-if (!function_exists('with')) {
-    /**
-     * Return the given object. Useful for chaining. This class has many influences from Joomla!Framework:
-     * https://github.com/joomla/joomla-framework/blob/staging/src/Joomla/PHP/methods.php
-     *
-     * This method provides forward compatibility for the PHP 5.4 feature Class member access on instantiation.
-     * e.g. (new Foo)->bar().
-     * See: http://php.net/manual/en/migration54.new-features.php
-     *
-     * @param   mixed $object The object to return.
-     *
-     * @since  2.0
-     *
-     * @return mixed
-     *
-     * @deprecated Use native syntax.
-     */
-    function with($object)
-    {
-        return $object;
+namespace {
+    if (!function_exists('with')) {
+        /**
+         * Return the given object. Useful for chaining. This class has many influences from Joomla!Framework:
+         * https://github.com/joomla/joomla-framework/blob/staging/src/Joomla/PHP/methods.php
+         *
+         * This method provides forward compatibility for the PHP 5.4 feature Class member access on instantiation.
+         * e.g. (new Foo)->bar().
+         * See: http://php.net/manual/en/migration54.new-features.php
+         *
+         * @param   mixed $object The object to return.
+         *
+         * @since  2.0
+         *
+         * @return mixed
+         *
+         * @deprecated Use native syntax.
+         */
+        function with($object)
+        {
+            return $object;
+        }
     }
-}
 
-if (!function_exists('show')) {
-    /**
-     * Dump Array or Object as tree node. If send multiple params in this method, this function will batch print it.
-     *
-     * @param   mixed $data Array or Object to dump.
-     *
-     * @since   2.0
-     *
-     * @return  void
-     */
-    function show($data)
-    {
-        $args = func_get_args();
+    if (!function_exists('show')) {
+        /**
+         * Dump Array or Object as tree node. If send multiple params in this method, this function will batch print it.
+         *
+         * @param   mixed $data Array or Object to dump.
+         *
+         * @since   2.0
+         *
+         * @return  void
+         */
+        function show($data)
+        {
+            $args = func_get_args();
 
-        $last = array_pop($args);
+            $last = array_pop($args);
 
-        if (is_int($last)) {
-            $level = $last;
-        } else {
-            $level = 5;
+            if (is_int($last)) {
+                $level = $last;
+            } else {
+                $level = 5;
 
-            $args[] = $last;
-        }
-
-        echo "\n\n";
-
-        if (PHP_SAPI !== 'cli') {
-            echo '<pre>';
-        }
-
-        // Dump Multiple values
-        if (count($args) > 1) {
-            $prints = [];
-
-            $i = 1;
-
-            foreach ($args as $arg) {
-                $prints[] = "[Value " . $i . "]\n" . \Windwalker\Utilities\ArrayHelper::dump($arg, $level);
-                $i++;
+                $args[] = $last;
             }
 
-            echo implode("\n\n", $prints);
-        } else {
-            // Dump one value.
-            echo \Windwalker\Utilities\ArrayHelper::dump($data, $level);
-        }
+            echo "\n\n";
 
-        if (PHP_SAPI !== 'cli') {
-            echo '</pre>';
+            if (PHP_SAPI !== 'cli') {
+                echo '<pre>';
+            }
+
+            // Dump Multiple values
+            if (count($args) > 1) {
+                $prints = [];
+
+                $i = 1;
+
+                foreach ($args as $arg) {
+                    $prints[] = "[Value " . $i . "]\n" . \Windwalker\Utilities\ArrayHelper::dump($arg, $level);
+                    $i++;
+                }
+
+                echo implode("\n\n", $prints);
+            } else {
+                // Dump one value.
+                echo \Windwalker\Utilities\ArrayHelper::dump($data, $level);
+            }
+
+            if (PHP_SAPI !== 'cli') {
+                echo '</pre>';
+            }
+        }
+    }
+
+    if (!function_exists('is_stringable')) {
+        /**
+         * is_stringable
+         *
+         * @param mixed $var
+         *
+         * @return  bool
+         *
+         * @since  3.5
+         */
+        function is_stringable($var): bool
+        {
+            return (is_scalar($var) && !is_bool($var)) || (is_object($var) && method_exists($var, '__toString'));
         }
     }
 }
 
-if (!function_exists('is_stringable')) {
+namespace Windwalker {
     /**
-     * is_stringable
+     * Do some operation after value get.
      *
-     * @param mixed $var
+     * @param mixed    $value
+     * @param callable $callable
      *
-     * @return  bool
+     * @return  mixed
      *
-     * @since  3.5
+     * @since  3.5.1
      */
-    function is_stringable($var): bool
+    function tap($value, callable $callable)
     {
-        return (is_scalar($var) && !is_bool($var)) || (is_object($var) && method_exists($var, '__toString'));
+        $callable($value);
+
+        return $value;
     }
 }
