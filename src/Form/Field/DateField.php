60a556dad4eaffc8950e4927b167d0101e1e2710
Simon Asika <asika32764@gmail.com>
2019-10-27 00:10:17 +0800

    [Form] Add DateField #657

---
+++ src/Form/Field/DateField.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+/**
+ * Part of earth project.
+ *
+ * @copyright  Copyright (C) 2019 LYRASOFT.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+namespace Windwalker\Form\Field;
+
+/**
+ * The DateField class.
+ *
+ * @since  3.5.14
+ */
+class DateField extends AbstractHtml5Field
+{
+    /**
+     * Property type.
+     *
+     * @var  string
+     */
+    protected $type = 'date';
+}
