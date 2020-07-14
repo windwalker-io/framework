835ad32b074d57422dc7bc784f76794c1c963b61
Simon Asika <asika32764@gmail.com>
2020-07-14 12:53:03 +0800

    Fix str bracket

--- src/String/phputf8/ord.php
+++ src/String/phputf8/ord.php
@@ -27,36 +27,36 @@ function utf8_ord($chr)
         return $ord0;
     }
 
-    if (!isset($chr{1})) {
+    if (!isset($chr[1])) {
         trigger_error('Short sequence - at least 2 bytes expected, only 1 seen');
 
         return false;
     }
 
-    $ord1 = ord($chr{1});
+    $ord1 = ord($chr[1]);
     if ($ord0 >= 192 && $ord0 <= 223) {
         return ($ord0 - 192) * 64
             + ($ord1 - 128);
     }
 
-    if (!isset($chr{2})) {
+    if (!isset($chr[2])) {
         trigger_error('Short sequence - at least 3 bytes expected, only 2 seen');
 
         return false;
     }
-    $ord2 = ord($chr{2});
+    $ord2 = ord($chr[2]);
     if ($ord0 >= 224 && $ord0 <= 239) {
         return ($ord0 - 224) * 4096
             + ($ord1 - 128) * 64
             + ($ord2 - 128);
     }
 
-    if (!isset($chr{3})) {
+    if (!isset($chr[3])) {
         trigger_error('Short sequence - at least 4 bytes expected, only 3 seen');
 
         return false;
     }
-    $ord3 = ord($chr{3});
+    $ord3 = ord($chr[3]);
     if ($ord0 >= 240 && $ord0 <= 247) {
         return ($ord0 - 240) * 262144
             + ($ord1 - 128) * 4096
@@ -64,12 +64,12 @@ function utf8_ord($chr)
             + ($ord3 - 128);
     }
 
-    if (!isset($chr{4})) {
+    if (!isset($chr[4])) {
         trigger_error('Short sequence - at least 5 bytes expected, only 4 seen');
 
         return false;
     }
-    $ord4 = ord($chr{4});
+    $ord4 = ord($chr[4]);
     if ($ord0 >= 248 && $ord0 <= 251) {
         return ($ord0 - 248) * 16777216
             + ($ord1 - 128) * 262144
@@ -78,7 +78,7 @@ function utf8_ord($chr)
             + ($ord4 - 128);
     }
 
-    if (!isset($chr{5})) {
+    if (!isset($chr[5])) {
         trigger_error('Short sequence - at least 6 bytes expected, only 5 seen');
 
         return false;
@@ -89,7 +89,7 @@ function utf8_ord($chr)
             + ($ord2 - 128) * 262144
             + ($ord3 - 128) * 4096
             + ($ord4 - 128) * 64
-            + (ord($chr{5}) - 128);
+            + (ord($chr[5]) - 128);
     }
 
     if ($ord0 >= 254 && $ord0 <= 255) {
