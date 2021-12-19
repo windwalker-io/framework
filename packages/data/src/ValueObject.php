d8dd0b4c9711dea4ecd7f599b1472dff862a420e
Simon Asika <asika32764@gmail.com>
2021-12-19 16:32:11 +0800

    Simple Data to handle json structure #920

---
+++ packages/data/src/ValueObject.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Part of earth project.
+ *
+ * @copyright  Copyright (C) 2021 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+namespace Windwalker\Data;
+
+use IteratorAggregate;
+use Windwalker\Utilities\Contract\ArrayAccessibleInterface;
+use Windwalker\Utilities\Contract\DumpableInterface;
+use Windwalker\Utilities\TypeCast;
+
+/**
+ * The ValueObject class.
+ */
+class ValueObject implements
+    ArrayAccessibleInterface,
+    DumpableInterface,
+    IteratorAggregate
+{
+    public function __construct(mixed $data)
+    {
+        $this->fill($data);
+    }
+
+    public function fill(mixed $data): static
+    {
+        $values = TypeCast::toArray($data);
+
+        foreach ($values as $key => $value) {
+            $this->$key = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetExists(mixed $key): bool
+    {
+        return property_exists($this, $key);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function &offsetGet(mixed $key): mixed
+    {
+        return $this->$key;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetSet(mixed $key, mixed $value): void
+    {
+        $this->$key = $value;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetUnset(mixed $key): void
+    {
+        unset($this->$key);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function dump(bool $recursive = false, bool $onlyDumpable = false): array
+    {
+        return TypeCast::toArray($this, $recursive, $onlyDumpable);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->dump());
+    }
+}
