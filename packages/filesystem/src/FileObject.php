eb21780a5ca519d66b47a63d81034278ebad2b66
Simon Asika <asika32764@gmail.com>
2022-02-06 00:40:52 +0800

    Fiux is link

--- packages/filesystem/src/FileObject.php
+++ packages/filesystem/src/FileObject.php
@@ -773,7 +773,7 @@ class FileObject extends SplFileInfo
         $isLink = parent::isLink();
 
         if (!$isLink && defined('PHP_WINDOWS_VERSION_BUILD')) {
-            $isLink = array_diff(stat($this->getPathname()) ?: [], lstat($this->getPathname()) ?: []) !== [];
+            $isLink = @readlink($this->getPathname()) !== $this->getPathname();
         }
 
         return $isLink;
