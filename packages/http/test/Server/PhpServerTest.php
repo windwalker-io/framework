07183f131707c6cc347320eb86c8fc822d95990b
Simon Asika <asika32764@gmail.com>
2023-04-23 14:44:43 +0800

    Fix test

--- packages/http/test/Server/PhpServerTest.php
+++ packages/http/test/Server/PhpServerTest.php
@@ -65,7 +65,9 @@ class PhpServerTest extends TestCase
         $this->instance = new HttpServer(
             [],
             new PhpServer(
-                $this->output = new MockOutput()
+                [],
+                null,
+                fn () => $this->output = new MockOutput()
             )
         );
     }
