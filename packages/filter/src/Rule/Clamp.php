cb2caba02f6d3998388bae1b6120c80a9696b6b6
Simon Asika <asika32764@gmail.com>
2023-10-29 21:36:26 +0800

    [Filter|Collection] Rename range to clamp #924

---
+++ packages/filter/src/Rule/Clamp.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) 2023 LYRASOFT.
+ * @license    MIT
+ */
+
+declare(strict_types=1);
+
+namespace Windwalker\Filter\Rule;
+
+use Windwalker\Filter\AbstractFilter;
+
+/**
+ * The Range class.
+ */
+class Clamp extends AbstractFilter
+{
+    protected int|float|null $min;
+
+    protected int|float|null $max;
+
+    /**
+     * RangeFilter constructor.
+     *
+     * @param  int|float|null  $min
+     * @param  int|float|null  $max
+     */
+    public function __construct(int|float|null $min = null, int|float|null $max = null)
+    {
+        $this->min = $min;
+        $this->max = $max;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function filter(mixed $value): mixed
+    {
+        if ($this->min !== null && $value < $this->min) {
+            $value = $this->min;
+        }
+
+        if ($this->max !== null && $value > $this->max) {
+            $value = $this->max;
+        }
+
+        return $value;
+    }
+}
