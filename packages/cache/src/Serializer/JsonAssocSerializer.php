d807ec2f4bf1dd507f12e806fa30b70b3e2765f0
Simon Asika <asika32764@gmail.com>
2025-06-03 22:08:40 +0800

    [Cache] JsonAssocSerializer #1181

---
+++ packages/cache/src/Serializer/JsonAssocSerializer.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Windwalker\Cache\Serializer;
+
+class JsonAssocSerializer implements SerializerInterface
+{
+    /**
+     * Encode data.
+     *
+     * @param  mixed  $data
+     *
+     * @return string|null
+     */
+    public function serialize(mixed $data): ?string
+    {
+        return json_encode($data);
+    }
+
+    /**
+     * Decode data.
+     *
+     * @param  string  $data
+     *
+     * @return mixed
+     */
+    public function unserialize(string $data): mixed
+    {
+        return json_decode($data, true);
+    }
+}
