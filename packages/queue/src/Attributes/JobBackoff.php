e78c0c9a88111de4ee48380130fdf9f4a79755d9
Simon Asika <asika32764@gmail.com>
2025-08-28 19:30:51 +0800

    more backoff params #1175

--- packages/queue/src/Attributes/JobBackoff.php
+++ packages/queue/src/Attributes/JobBackoff.php
@@ -14,13 +14,34 @@ class JobBackoff
     {
     }
 
-    public static function backoff(int $current, int $base = 1, ?int $maxTimes = null, int $pow = 2): int|false
-    {
-        if ($maxTimes !== null && $current >= $maxTimes) {
+    /**
+     * - linear = pow: 1 | (1, 2, 3, 4, 5, 6, ...)
+     * - exponential = pow: 2 | (1, 2, 4, 8, 16, 32, ...)
+     * - polynomial = pow: >= 3 | (1, 2, 9, 27, 81, 243, ...)
+     * - adaptive = pow: >= 2, adaptive: > 1 | (1, 4, 9, 16, 25, 36, ...)
+     *
+     * @param  int       $n
+     * @param  int       $base
+     * @param  int       $pow
+     * @param  int|null  $maxTimes
+     * @param  int       $maxSeconds
+     * @param  int       $adaptive
+     *
+     * @return  int|false
+     */
+    public static function backoff(
+        int $n,
+        int $base = 1,
+        int $pow = 2,
+        ?int $maxTimes = null,
+        int $maxSeconds = 86400,
+        int $adaptive = 1,
+    ): int|false {
+        if ($maxTimes !== null && $n >= $maxTimes) {
             return false;
         }
 
-        return $base * ($pow ** $current);
+        return min($maxSeconds, $base * ($pow ** $n) * $adaptive);
     }
 
     public static function resolve(int $attempts, array|int|null|false $backoff): int|null|false
