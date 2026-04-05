ffbb2874367bf0d554840f089e3811a8afecf7b5
Simon Asika <asika32764@gmail.com>
2026-04-05 11:37:43 +0800

    Allow string cursor field

--- packages/query/src/Data/QueryPaginate.php
+++ packages/query/src/Data/QueryPaginate.php
@@ -6,14 +6,40 @@ namespace Windwalker\Query\Data;
 
 use Windwalker\Query\Query;
 
-readonly class QueryPaginate
+class QueryPaginate
 {
+    public \Closure $nextHandler {
+        get => $this->getNextHandler();
+    }
+
+    protected string|\Closure $cursorField;
+
     public function __construct(
         public int $length,
         /**
-         * @var \Closure(Query $query): ?Query
+         * @var string|\Closure(Query $query): ?Query
          */
-        public \Closure $nextHandler,
+        \Closure|string $cursorField
     ) {
+        $this->cursorField = $cursorField;
+    }
+
+    protected function getNextHandler(): \Closure
+    {
+        $field = $this->cursorField;
+
+        if (is_string($field)) {
+            $field = static function (Query $query, object|array $item) use ($field) {
+                if (is_array($item)) {
+                    $cursor = $item[$field];
+                } else {
+                    $cursor = $item->{$field};
+                }
+
+                return $query->where($field, '>', $cursor);
+            };
+        }
+
+        return $field;
     }
 }
